82578c494dfdccc0cfb1dd0bf1393cc68fd32467
Holden B <holden.bucher.92@gmail.com>
2019-01-21 14:01:03 -0600

    remove files

--- index.js
+++
@@ -1,5 +0,0 @@
-const server = require("./server/server");
-
-const port = 5000;
-
-server.listen(port, () => console.log(`Server up and listening on ${port}`));
--- server/middleWare.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  authenticated: (req, res, next) => {
-    if (req.session && req.session.cookie) {
-      next();
-    } else {
-      res.status(401).json({ message: "you shall not pass!!" });
-    }
-  }
-};
