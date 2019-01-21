4d13de209fdd85492a61b29556234bd8979cbfaf
Holden B <holden.bucher.92@gmail.com>
2019-01-21 12:25:43 -0600

    cleaned up backend

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-// const express = require("express");
-// const cors = require("cors");
-// const helmet = require("helmet");
-// // const bcrypt = require("bcryptjs");
-// // const db = require("./database/dbConfig");
-// const session = require("express-session");
 const server = require("./server/server");
-
-// const server = express();
-// server.use(cors());
-// server.use(express.json());
-// server.use(helmet());
-// server.use(
-//   session({
-//     name: "notsession",
-//     secret: "nobody tosses a dwarf!",
-//     cookie: {
-//       maxAge: 1 * 24 * 60 * 60 * 1000,
-//       secure: true
-//     },
-//     httpOnly: true,
-//     resave: false,
-//     saveUninitialized: false
-//   })
-// );
 
 const port = 5000;
 
