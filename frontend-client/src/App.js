28692ff688903cbddd9116c082d7f876bac4d435
Holden B <holden.bucher.92@gmail.com>
2019-01-24 15:12:10 -0600

    bring react app back in

---
+++ frontend-client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import AppView from "./Views/AppView";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppView />
+      </div>
+    );
+  }
+}
+
+export default App;
