e4ba611a0efb772a922f977574afeff52d64a161
Holden B <holden.bucher.92@gmail.com>
2019-01-24 15:34:53 -0600

    one more one last thing

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const express = require('express');
+const path = require('path');
+
 const server = require('./server/server');
 
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`Server up and listening on ${port}`));
+
+server.use(express.static(path.join(__dirname, 'frontend-client/build')));
+
+server.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/frontend-client/build/index.html'));
+});
